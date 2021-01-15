2cadc3bbb40d79f9b1f428f65069a1b33ff8a866
graphictrooper <graphictrooper@gmail.com>
2021-01-15 09:24:11 +0900

    Update sdk_demo.js

--- js/sdk_demo.js
+++ js/sdk_demo.js
@@ -6,7 +6,6 @@ function sdk_demo(){
     html += 'LiskHQがピックアップしたSDKを使用した概念実証（PoC）ブロックチェーンアプリケーション・リスト。PoCに関してはこのページを参照してください。';
     html += '支払い、サプライチェーン、IoT、DAO、ゲーム、アーカイブ、ステーブルコインなどのPoCが紹介されています。<br>';
     html += '<a class="btn btn-bg-blue mt-3" href="https://lisk.io/apps" role="button">Lisk PoC List</a><br><br>';
-    html += '<img src="img/poc.jpg">';
     html += '</div>';
     html += '<!-------Section 3 End-------></div>';
 document.write(html);
