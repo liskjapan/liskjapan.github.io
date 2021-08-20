7775be2d93d0eeefdf8b5a8a7345ff7ec777f535
graphictrooper <graphictrooper@i.softbank.jp>
2021-08-21 07:26:41 +0900

    Add files via upload

--- js/header.js
+++ js/header.js
@@ -1,16 +1,7 @@
 function header(){
     var html = "";
     html += '<nav class="navbar navbar-expand-md navbar-dark fixed-top header-bar">';
-    html += '<a class="navbar-brand" href="index.html"><img alt="Lisk Japan" src="img/lisk_japan_logo-white.svg" width="120"></a>';
-    html += '<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation"><i class="fas fa-bars"></i></button>';
-    html += '<div class="collapse navbar-collapse pt-3" id="navbarCollapse">';
-    html += '<ul class="navbar-nav mx-auto">';
-    html += '<li class="nav-item active pt-1 pl-4 h6"><a class="nav-link ml-3" href="index.html">TOP</a></li>';
-    html += '<li class="nav-item active pt-1 pl-4 h6"><a class="nav-link ml-3" href="poc-liskbike.html">概念実証</a></li>';
-    html += '<li class="nav-item active pt-1 pl-4 h6"><a class="nav-link ml-3" href="what-is-lisk.html?ref=01&btn=02">Liskとは?</a></li>';
-    html += '<li class="nav-item active pt-1 pl-4 pr-5 h6"><a class="nav-link ml-3" href="enterprise.html">企業での活用</a></li>';
-    html += '</ul>';
-    html += '</div>';
+    html += '<a class="navbar-brand" href="index.html"><img alt="Lisl Business Association" src="img/lisk_japan_logo-white.svg" height="30"></a>';
     html += '</nav>';
 document.write(html);
 }
--- js/headerbutton.js
+++ js/headerbutton.js
@@ -1,10 +1,8 @@
 function headerbutton(){
     var html = "";
     html += '<br>';
-    html += '<a class="btn btn-bg-blue mt-5 pl-5 pr-5 pt-3 pb-2 rounded-pill" href="https://lisk.io/wallet" role="button">';
-    html += '<h6>Download wallet <i class="fas fa-external-link-alt"></i></h6>';
+    html += '<a class="btn glm-btn-bg-blue-top mt-5 pl-5 pr-5 pt-3 pb-2 rounded-pill" href="#" role="button">';
+    html += '<h6><span style="font-weight:600;">ボタン</span></h6>';
     html += '</a>';
-    html += '<br>';
-    html += '<div class="pt-2 lisk-font-top">for iOS, Android, Mac, Windows, Linux</div>';
 document.write(html);
 }
